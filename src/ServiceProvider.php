c794143c8167e79854d199e13d8af635be3cb1da
Maksim Mikhailov <2618094@gmail.com>
2023-01-28 00:24:13 +0300

    init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kirbykot\DistributedEvents;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
